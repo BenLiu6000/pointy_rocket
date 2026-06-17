8d1caaf738b317d596d351e370219314191efadc
Basil Amin <184784851+BasilAmin@users.noreply.github.com>
2026-06-17 23:06:21 +0100

    Add files via upload
    Fixed flight firmware directory

---
+++ Dictator_Flight_Firmware/src/main.cpp
@@ -0,0 +1,24 @@
+/*
+THE DICTATOR FLIGHT FIRMWARE
+
+*/
+
+// Dependancies
+#include <Arduino.h>
+#include <SD.h>
+#include <I2Cdev.h>
+#include <Wire.h>
+#include <MPU6050.h>
+#include <Adafruit_MPL3115A2.h>
+#include <Adafruit_NeoPixel.h>
+#include <Servo.h>
+
+
+
+void setup(){
+
+}
+
+void loop(){
+  
+}
